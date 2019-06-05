0581252fbdbddda122be8747a6e952c0a9d610c6
hecmerod <47757085+hecmerod@users.noreply.github.com>
2019-06-05 13:18:30 +0200

    crono

--- src/crossfitgym/Controllers/CronoController.java
+++ src/crossfitgym/Controllers/CronoController.java
@@ -1,14 +1,16 @@
 package crossfitgym.Controllers;
 
-import crossfitgym.Classes.Crono;
 import crossfitgym.Classes.SesionTipo;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.application.Platform;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
+import javafx.concurrent.Service;
+import javafx.concurrent.Task;
 import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -31,12 +33,7 @@ public class CronoController implements Initializable{
     private Insets x2;
     @FXML
     private Insets x3;
-
-    private SesionTipo sTipo;
     
-    private int nGrupo;
-    @FXML
-    private Label timer;
     @FXML
     private Font x6;
     @FXML
@@ -47,23 +44,33 @@ public class CronoController implements Initializable{
     private Insets x4;
     @FXML
     private Insets x5;
+    @FXML
+    private Insets x7;
+    
+    @FXML
+    private Label timer;
+    @FXML
+    private Button start_pause;
     
     @FXML
     private ImageView ej0, ej1, ej2, ej3, ej4, ej5, ej6, ej7, ej8, ej9, ej10,
             ej11, ej12, ej13;
     private ImageView[] ej;
-    @FXML
-    private Insets x7;
+
     
-    private boolean isStarted = false;
-    private boolean isPaused;
+    private static final int DELAY = 50;
+    
+    private static long prevTime = 0, startTime = 0, stopTime = 0;
+ 
+    private boolean firstTime = true, stopped = true, paused = false;
     
     private int contEj = 0;
     
-    private Crono crono;
+    private MyService crono;
     
-    @FXML
-    private Button start_pause;
+    private int nGrupo;
+    private SesionTipo sTipo;    
+    
     
     private StringProperty tiempo = new SimpleStringProperty();
     
@@ -73,16 +80,17 @@ public class CronoController implements Initializable{
         ej[0] = ej0; ej[1] = ej1; ej[2] = ej2; ej[3] = ej3; ej[4] = ej4;
         ej[5] = ej5; ej[6] = ej6; ej[7] = ej7; ej[8] = ej8; ej[9] = ej9;
         ej[10] = ej10; ej[11] = ej11; ej[12] = ej12; ej[13] = ej13;
-        
-        this.crono = new Crono(this.tiempo);
-        
+                
         tiempo.setValue("00:00");
         timer.textProperty().bind(tiempo);
+        
+        crono = new MyService();
     }    
     
     public void initStage(Stage s, SesionTipo sT, int nG) {
         this.stage = s; this.sTipo = sT; this.nGrupo = nG;     
         setImages();
+        System.err.println(this.sTipo.getTEjercicios());
     }
     
     private void setImages() {
@@ -100,35 +108,116 @@ public class CronoController implements Initializable{
             }
         }
     }
-
-    @FXML
-    private void stop(ActionEvent event) {
-        isStarted = false;
-        start_pause.setText("PLAY");
-    }
-
-    @FXML
-    private void next(ActionEvent event) {
+    @FXML private void next(ActionEvent event) {
         contEj++;
     }
+    
+    @FXML
+    private void stop(ActionEvent event) {   
+        if(paused) tiempo.setValue("00:00");
+        crono.cancel();
+        startTime = prevTime = stopTime = 0;                
+        stopped = true; paused = false;
+        start_pause.setText("PLAY");
+    }   
+    
 
     @FXML
     private void start_pause(ActionEvent event) {        
-        if(!isStarted) {            
+        if(firstTime) { 
+            crono.start(); firstTime = false; 
             start_pause.setText("PAUSE");
-            isStarted = true;
-            
-            this.crono.start();
         }
-        else if (isPaused){
+        else if (stopped) { 
+            tiempo.setValue("00:00");         
+            crono.restart(); 
             start_pause.setText("PAUSE");
-            isPaused = false;
+        }
+        else if (!paused) {
+            paused = true;
+            System.err.println("pausa");
+            crono.cancel();
+            start_pause.setText("PLAY");
         }
         else {
-            start_pause.setText("PLAY");
-            isPaused = true;
-        }  
-    }  
-   
+            paused = false;
+            crono.restart();
+            start_pause.setText("PAUSE");
+        }
+    }
+    
+    
+   private class MyService extends Service<Void> {
+
+        @Override
+        protected Task<Void> createTask() {
+            return new Task<Void>() {
+                
+                void calcula() {                    
+                    long nowTime = System.currentTimeMillis();
+                    Long totalTime = (nowTime - startTime) - stopTime;
+                    prevTime = nowTime;
+
+                    final Integer minutos = totalTime.intValue() / 60000;
+                    final Integer resto = totalTime.intValue() % 60000;
+                    final Integer segundos = resto / 1000;
+                    final Integer centesimas = resto % 60;
+                    
+                    if(segundos.compareTo(sTipo.getTEjercicios()) > 0 ) {    
+                        contEj++;
+                        startTime = prevTime = stopTime = 0;
+                        stopped = true; paused = false;
+                        start_pause.setText("PLAY");
+                        
+                        this.cancel();                        
+                    }
+                    else if(minutos == 0)
+                    Platform.runLater(() -> {
+                        tiempo.setValue(String.format("%02d", segundos) + ":"
+                                        + String.format("%02d", centesimas));
+                    });
+                    else Platform.runLater(() -> {
+                        tiempo.setValue(String.format("%02d", minutos) + ":"
+                                + String.format("%02d", segundos));
+                    });
+
+                    
+                }
+
+                @Override
+                protected Void call() {
+                    if (stopped) {
+                        startTime = System.currentTimeMillis();
+                        prevTime = startTime;
+                        stopped = false;
+                    } else {
+                        long nowTime = System.currentTimeMillis();
+                        Long elapsedTime = nowTime - prevTime;
+                        stopTime = stopTime + elapsedTime;
+                    }
+                    while (true) {
+                        try {
+                            Thread.sleep(DELAY);
+                        } catch (InterruptedException ex) {
+                            if (isCancelled()) {
+                                break;
+                            }
+                        }
+                        if (isCancelled()) {
+                            break;
+                        }
+                        calcula();
+                    }
+                    return null;
+                }
+
+                @Override
+                protected void cancelled() {
+                    super.cancelled();
+                    prevTime = System.currentTimeMillis();                    
+                }
+            };
+        }
+    }
     
 }
